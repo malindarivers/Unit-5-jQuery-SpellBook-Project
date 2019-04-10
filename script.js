589cc8a6055a5b5832df25eb68b78bd23b2a8a62
malindarivers <43344616+malindarivers@users.noreply.github.com>
2019-04-10 09:45:25 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+//Spell #1
+
+$(" #Book-button ").click(function(){
+    $("#Book").fadeOut();
+ 
+});
+
+
+
+//Spell #2
+
+$("#Levitation-button").click(function(){
+    $("#Levitation").hide();
+ 
+});
+
+
+
+//Spell #3
+
+$("#appear-button").click(function(){
+    $("#Appear").toggle();
+ 
+});
+
+
+
